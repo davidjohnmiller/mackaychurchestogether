9e1b2df88c5aab796ea442d86cf4f1bad10f064e
David Miller <davidmiller@live.com.au>
2016-04-09 18:35:54 +1000

    Define bounds statically

--- javascripts/map.js
+++ javascripts/map.js
@@ -1,6 +1,11 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiZGF2aWRqb2hubWlsbGVyIiwiYSI6ImNpbXFidWJzbTAwZm12N2trOTIxaTl3YXQifQ.JaRYbyll5SlOJ1WqY1G7GQ';
+// 1deg around -21.14121, 149.185625
+var southWest = L.latLng(-22.14121, 148.185625),
+    northEast = L.latLng(-20.14121, 150.185625),
+    bounds = L.latLngBounds(southWest, northEast);
 var map = L.mapbox.map('map', 'mapbox.streets', {
-minZoom: 10
+  maxBounds: bounds,
+  minZoom: 10
 });
 var featureLayer = L.mapbox.featureLayer()
 .loadURL('churches.geojson')
