64ef009a2d29c949f89b8c080e27fff72351dde5
David Miller <davidmiller@live.com.au>
2016-04-09 20:12:35 +1000

    Remove static minZoom

--- javascripts/map.js
+++ javascripts/map.js
@@ -1,7 +1,5 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiZGF2aWRqb2hubWlsbGVyIiwiYSI6ImNpbXFidWJzbTAwZm12N2trOTIxaTl3YXQifQ.JaRYbyll5SlOJ1WqY1G7GQ';
-var map = L.mapbox.map('map', 'mapbox.streets', {
-	minZoom: 8
-});
+var map = L.mapbox.map('map', 'mapbox.streets');
 var featureLayer = L.mapbox.featureLayer()
 .loadURL('churches.geojson')
 .on('ready', function() {
